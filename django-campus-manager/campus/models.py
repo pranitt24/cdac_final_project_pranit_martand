a0de8ef2395172ab0b0af73b88484f584b83f413
martandjadhav <martandj2006@gmail.com>
2026-06-20 17:39:46 +0530

    Add Course M2M relationship and Bootstrap 5 UI

--- django-campus-manager/campus/models.py
+++ django-campus-manager/campus/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+# 1. Create the Course Table
+class Course(models.Model):
+    course_code = models.CharField(max_length=20, unique=True)
+    course_name = models.CharField(max_length=100)
+    credits = models.IntegerField(default=3)
+
+    def __str__(self):
+        return f"{self.course_code} - {self.course_name}"
+
+# 2. Update the Student Table
 class Student(models.Model):
     enrollment_number = models.CharField(max_length=20, unique=True)
     first_name = models.CharField(max_length=50)
@@ -7,6 +17,8 @@ class Student(models.Model):
     email = models.EmailField(unique=True)
     department = models.CharField(max_length=100)
     
-    # This controls how the object looks in the Django Admin panel
+
+    courses = models.ManyToManyField(Course, blank=True, related_name='enrolled_students')
+    
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.enrollment_number})"
--- django-campus-manager/campus/views.py
+++ django-campus-manager/campus/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Student
 
-# Create your views here.
+# This simple tag acts as a bouncer for this specific page
+@login_required
+def student_directory(request):
+    all_students = Student.objects.all()
+    context = {'students': all_students}
+    return render(request, 'campus/student_directory.html', context)
+
+def student_directory(request):
+    # Fetch all students from the database
+    all_students = Student.objects.all()
+    
+    # Pass them to an HTML file (which we will create next)
+    context = {'students': all_students}
+    return render(request, 'campus/student_directory.html', context)
